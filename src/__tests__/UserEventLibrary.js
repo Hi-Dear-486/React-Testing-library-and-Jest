18d4af5681fb12685212851ee4741e3804037d90
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-12-10 22:27:27 +0500

    UserEventLibrary

--- src/__tests__/UserEventLibrary.js
+++ src/__tests__/UserEventLibrary.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import UserEventLibrary from "../UserEventLibrary";
 
@@ -6,7 +6,9 @@ test("UserEventLibrary", async () => {
   userEvent.setup();
   render(<UserEventLibrary />);
   const btn = screen.getByRole("button");
-  await userEvent.click(btn);
+  await act(async () => {
+    await userEvent.click(btn);
+  });
   expect(
     screen.getByText("Click Event with User Event Library")
   ).toBeInTheDocument();
